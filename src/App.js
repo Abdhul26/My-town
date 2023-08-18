25d225848e05b9523d74b7d62cd16f7a3ffea89c
Abdul Ghani <75516919+Abdhul26@users.noreply.github.com>
2023-08-18 19:55:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg'
+import './App.css'
+import PrimarySearchAppBar from './Components/NavBar/PrimarySearchAppBar'
+import TemporaryDrawer from './Components/SideBar/TemporaryDrawer'
+function App() {
+  return (
+    <div className='App'>
+      {/* <header className='App-header'>MY-TOWN PROJECT</header> */}
+      <PrimarySearchAppBar />
+    </div>
+  )
+}
+
+export default App
